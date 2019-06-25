f9f07aeb38c7faab2d400472de4ceddad4a0deb6
Selin Huang <selin.hyx@gmail.com>
2019-06-25 21:48:50 +0000

    created flatten array function to flatten inner arrays within an array

---
+++ flatten.js
@@ -0,0 +1,33 @@
+const eqArrays = function(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    while (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅ Assertion Passed`);
+  } else {
+    console.log(`❌ Assertion Failed`);
+  }
+};
+
+const flatten = function(arr) {
+  let output = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      for (let k = 0; k < arr[i].length; k++) {
+        output.push(arr[i][k]);
+      }
+    } else {
+      output.push(arr[i]);
+    }
+  }
+  return output;
+};
+
+console.log(flatten([1, 2, [3, 4], 5, [6]])); // => [1, 2, 3, 4, 5, 6]
+assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
