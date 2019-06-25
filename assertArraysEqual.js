445f0dc55114064ff4f58c055f364787d331c3f6
Selin Huang <selin.hyx@gmail.com>
2019-06-25 19:22:44 +0000

    created assertArrayEqual function to directly compare two arrays

---
+++ assertArraysEqual.js
@@ -0,0 +1,23 @@
+
+const eqArrays = function(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    while (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅ Assertion Passed`);
+  } else {
+    console.log(`❌ Assertion Failed`);
+  }
+};
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
+assertArraysEqual([1, 2, 3], [3, 2, 1]); // => false
+
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => true
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => false
