30c818d42897e2463c1361f92ff946e526dd92b6
Selin Huang <selin.hyx@gmail.com>
2019-06-26 13:30:10 +0000

    implemented countLetters function to count letter occurrences in a string without countint the spaces

---
+++ countLetters.js
@@ -0,0 +1,50 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(string) {
+  string = string.split(" ");
+  string = string.join("");
+  let output = {};
+  for (const letters of string) {
+    if (output[letters]) {
+      output[letters] += 1;
+    } else {
+      output[letters] = 1;
+    }
+  }
+  return output;
+};
+
+
+console.log(countLetters("lighthouse in the house"));
+// {
+//   l: 1,
+//   i: 2,
+//   g: 1,
+//   h: 4,
+//   t: 2,
+//   o: 2,
+//   u: 2,
+//   s: 2,
+//   e: 3,
+//   n: 1,
+// }
+
+let result = countLetters("lighthouse in the house");
+assertEqual(result["l"], 1);
+assertEqual(result["i"], 2);
+assertEqual(result["g"], 1);
+assertEqual(result["h"], 4);
+assertEqual(result["t"], 2);
+assertEqual(result["o"], 2);
+assertEqual(result["u"], 2);
+assertEqual(result["s"], 2);
+assertEqual(result["e"], 3);
+assertEqual(result["n"], 1);
+
+
