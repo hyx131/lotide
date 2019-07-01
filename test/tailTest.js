255fc1ff6c354545b33dc160b502006ad7eb6b8e
Selin Huang <selin.hyx@gmail.com>
2019-07-01 14:18:09 +0000

    refactored to use mocha/chai unit testing

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,11 +1,21 @@
-const assertEqual = require("../assertEqual");
+// const assertEqual = require("../assertEqual");
+// const tail = require("../tail");
+
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// tail(words);
+// assertEqual(words.length, 3);
+
+// const result = tail([]);
+// assertEqual(result.length, 0);
+// // assertEqual(result[0], "Lighthouse");
+// // assertEqual(result[1], "Labs");
+
+
+const assert = require("chai").assert;
 const tail = require("../tail");
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
-
-const result = tail([]);
-assertEqual(result.length, 0);
-// assertEqual(result[0], "Lighthouse");
-// assertEqual(result[1], "Labs");
+describe("#tail", () => {
+  it(`returns "Labs" for ["Yo Yo", "Lighthouse", "Labs"]`, () => {
+    assert.deepEqual(tail(["Yo Yo", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  });
+});
