fbb741f92350c85bf7ffa4574ffae65ef179bd87
Selin Huang <selin.hyx@gmail.com>
2019-07-01 17:31:59 +0000

    changed to ES6 syntax

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const takeUntil = require('./takeUntil');
 const map = require('./map');
 const letterPositions = require('./letterPositions');
 const flatten = require('./flatten');
-const findKeyByValue = require('./findKeyByValue');
+const findKeyByValue = require('./findKeyBy');
 const findKey = require('./findKey');
 const eqArrays = require('./eqArrays');
 const eqObjects = require('./eqObjects');
@@ -16,22 +16,42 @@ const assertEqual = require('./assertEqual');
 const assertObjectsEqual = require('./assertObjectsEqual');
 const assertArraysEqual = require('./assertArraysEqual');
 
+// module.exports = {
+//   head: head,
+//   tail: tail,
+//   middle: middle,
+//   assertArraysEqual : assertArraysEqual,
+//   assertEqual : assertEqual,
+//   assertObjectsEqual : assertObjectsEqual,
+//   countLetters : countLetters,
+//   countOnly : countOnly,
+//   eqArrays : eqArrays,
+//   eqObjects: eqObjects,
+//   findKey : findKey,
+//   findKeyByValue : findKeyByValue,
+//   flatten: flatten,
+//   letterPositions: letterPositions,
+//   map : map,
+//   takeUntil : takeUntil,
+//   without : without
+// };
+
 module.exports = {
-  head: head,
-  tail: tail,
-  middle: middle,
-  assertArraysEqual : assertArraysEqual,
-  assertEqual : assertEqual,
-  assertObjectsEqual : assertObjectsEqual,
-  countLetters : countLetters,
-  countOnly : countOnly,
-  eqArrays : eqArrays,
-  eqObjects: eqObjects,
-  findKey : findKey,
-  findKeyByValue : findKeyByValue,
-  flatten: flatten,
-  letterPosition: letterPosition,
-  map : map,
-  takeUntil : takeUntil,
-  without : without
+  head,
+  tail,
+  middle,
+  assertArraysEqual,
+  assertEqual,
+  assertObjectsEqual,
+  countLetters,
+  countOnly,
+  eqArrays,
+  eqObjects,
+  findKey,
+  findKeyByValue,
+  flatten,
+  letterPositions,
+  map,
+  takeUntil,
+  without
 };
