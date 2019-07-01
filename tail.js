032cd05858c740babbab54d128194b05c988985d
Selin Huang <selin.hyx@gmail.com>
2019-07-01 11:10:55 +0000

    created separate test file for tail function

--- tail.js
+++ tail.js
@@ -1,21 +1,8 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require("./assertEqual");
 
 const tail = function(arr) {
   let arrTail = arr.slice(1);
   return arrTail;
 };
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
-
-const result = tail([]);
-assertEqual(result.length, 0);
-// assertEqual(result[0], "Lighthouse");
-// assertEqual(result[1], "Labs");
+module.exports = tail;
